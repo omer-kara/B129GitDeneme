fac73b8a150939db546eb63594c2cd91e366cac2
Omer Kara <fomerkara1@gmail.com>
2023-02-25 16:51:11 +0100

    v3

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -1,8 +1,5 @@
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhabe");
-        System.out.println("Dunya");
     }
 }
-//restore ==> Tamamen Working Space'i temizler
-//Stash ==> Hafizaya aliyor
