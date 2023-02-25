49739421d6b6dfdbd257c98c53c7714c76fb89eb
Omer Kara <fomerkara1@gmail.com>
2023-02-25 17:28:15 +0100

    v4

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -2,6 +2,8 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhabe");
         System.out.println("Dunya");
+        System.out.println("B129");
+
     }
 }
 //restore ==> Tamamen Working Space'i temizler
